0b8ce5515b6d41b252c718aa39696b5c5380a168
jerishaemerson <jerishaemerson97@gmail.com>
2021-12-27 10:03:54 +0530

    auto carousel

--- mystore/src/app/app.module.ts
+++ mystore/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StoreModule } from "@ngrx/store";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomAddToCartModule } from "./custom-add-to-cart/custom-add-to-cart.module";
+import { CustomProductCarouselModule } from "./custom-product-carousel/custom-product-carousel.module";
 import { SpartacusModule } from './spartacus/spartacus.module';
 
 @NgModule({
@@ -19,6 +20,7 @@ import { SpartacusModule } from './spartacus/spartacus.module';
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     SpartacusModule,
+    CustomProductCarouselModule,
     CustomAddToCartModule,
   ],
   providers: [],
